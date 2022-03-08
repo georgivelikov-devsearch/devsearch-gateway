dc88c609296889c0d6f0f940c77769d32a1b2bb1
georgivelikov-devsearch <gmvelikov1@gmail.com>
2022-03-08 21:36:03 +0200

    Initial commit

---
+++ devsearch-gateway/src/main/java/devsearch/gateway/DevsearchGatewayApplication.java
@@ -0,0 +1,13 @@
+package devsearch.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsearchGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsearchGatewayApplication.class, args);
+	}
+
+}
